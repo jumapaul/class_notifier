3a54ce0bc2637166e2243e37afc412060384a00e
jumapaul <pauljuma736@gmail.com>
2022-11-29 21:29:28 +0300

    first commit

---
+++ app/src/main/java/com/example/classreminder/fragments/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.classreminder.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.classreminder.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        view.btnSchedules.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment2_to_scheduleFragment)
+        }
+
+        return view
+    }
+}
